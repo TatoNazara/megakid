13f94248d7334b18c9e3c8858bda9aa06dc7709d
TatoNazara <2018robota@gmail.com>
2023-01-10 22:41:29 +0100

    First commit

---
+++ src/TatoNazara.Megakid/TatoNazara.Megakid.Web/Program.cs
@@ -0,0 +1,57 @@
+﻿using Serilog;
+using Serilog.Events;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("logs/Main_Log.txt",
+                LogEventLevel.Information,
+                rollingInterval: RollingInterval.Day,             //за 10 последних дней хранится Information лог (100МБ лимит размера файла)
+                retainedFileCountLimit: 10,
+                fileSizeLimitBytes: 100000000,
+                rollOnFileSizeLimit: true)
+    .CreateLogger();
+
+try
+{
+    Log.Information("Starting web application");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog(); // <-- Add this line
+
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
+
+
